9e7fd8714fc656a80d7a87a5a6cbb18f23c3788c
HarshilGem <jetaniharshil0810@gmail.com>
2025-08-28 11:21:49 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 
 import express from 'express';
-import cors from 'cors';
 import connectDB from './config/db.js';
-import itemRoutes from './routes/itemRoutes.js';
-import { errorHandler } from './middlewares/errorHandler.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,16 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
 
-// Routes
-app.use('/api/items', itemRoutes);
-
-// Error handler
-app.use(errorHandler);
-
-// Connect to DB and start server
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
